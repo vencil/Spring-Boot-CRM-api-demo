faf0cad3bcdbf72dcd81aa406dd391efe6c74fe5
poyu <vencsvencil@gmail.com>
2022-04-12 11:23:51 +0800

    Commit missing annotations

--- src/main/java/com/poyu/demo/controller/api/ClientController.java
+++ src/main/java/com/poyu/demo/controller/api/ClientController.java
@@ -8,6 +8,7 @@ import com.poyu.demo.domain.dto.req.ClientRequest;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +24,7 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping(value = "/api/client", produces = MediaType.APPLICATION_JSON_VALUE)
+@Validated
 public class ClientController {
 
     @GetMapping("/{id}")
--- src/main/java/com/poyu/demo/controller/api/CompanyController.java
+++ src/main/java/com/poyu/demo/controller/api/CompanyController.java
@@ -9,6 +9,7 @@ import com.poyu.demo.util.Util;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/api/company", produces = MediaType.APPLICATION_JSON_VALUE)
+@Validated
 public class CompanyController {
 
     @GetMapping("/{id}")
